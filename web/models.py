c93f25dee5b64fbdf5fccfe07559f7f2f93eb48d
Rasool Fazeli <f.rasool66@gmail.com>
2024-07-22 16:24:52 +0330

    add my expense model and add some expens into table

--- web/models.py
+++ web/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.translation import gettext as _
 
 # Create your models here.
+
+# kharj
+class Expense(models.Model):
+    title = models.CharField(_("عنوان"), max_length=255)
+    date = models.DateTimeField(_("تاریخ"))
+    amount = models.BigIntegerField(_("مقدار"))
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+            
